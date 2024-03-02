3744fb05e6bf16ad402671205c0d5d9c46a861af
Jjateen <jjateen@gmail.com>
2024-03-02 15:58:13 +0530

    navbar done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,26 @@
 import React, { useEffect } from 'react';
+import Navbar from './Components/Common/Navbar';
+import { BrowserRouter,Routes,Route} from 'react-router-dom';
+import HomePage from "./Pages/HomePage";
+import ChatPage from './Pages/ChatPage';
+import ClassifyPage from './Pages/ClassifyPage';
+import ResultPage from "./Pages/ResultPage";
+import AboutUs from './Pages/AboutUs';
 
 const App = () => {
     return (
-        <div>
-            App
-        </div>
+        <>
+        <Navbar />
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<HomePage />}></Route>
+            <Route path="/chat" element={<ChatPage />}></Route>
+            <Route path="/classify" element={<ClassifyPage />}></Route>
+            <Route path="/classify-result" element={<ResultPage />}></Route>
+            <Route path="/about-us" element={<AboutUs />}></Route>
+        </Routes>
+        </BrowserRouter>
+        </>
     );
 }
 
